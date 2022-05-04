0f2815e43c6cbb75e7406051e64eafb10d22d185
jinyoungkimm <100742333+jinyoungkimm@users.noreply.github.com>
2022-05-04 10:48:53 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+  include_pacakge_data=True,
+  name = 'jinyoungdonga',
+  version = '1.0.0'
+  description = 'oss-devolopment class test output',
+  author_email='wlsdud6523@kakao.com',
+  url='https://github.com/jinyoungkimm/testout',
+  install require = ['pytest'],
+  long_description= 'oss_development...',
+  long description_content_type = 'text/markdown',
+  classifiers = [
+    "programming Language ::Python ::3",
+    "Operation System :: OS,
+  ],
+)
